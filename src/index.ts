e2cc72a017e07de92185a133e3363ed0965bd9d0
Carter Himmel <carter@elhnet.net>
2021-08-02 16:22:24 -0600

    feat: use logger in fastify

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ function verify(req: FastifyRequest, reply: FastifyReply, done: () => void) {
 }
 
 function start() {
-	const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify();
+	const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({ logger });
 
 	server.post('/interactions', { preHandler: verify }, async (req, res) => {
 		logger.info(`incoming request: `, req.body);
